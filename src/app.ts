24765ba3b1597bc2ff317afab5fd68fbfa121f14
Sahithi <sahithi.maligireddy@zemosolabs.com>
2025-10-24 18:28:14 +0530

    fix: cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
-import express from "express";
-import ticketRoutes from "./tickets/ticket.routes";
+import express from 'express';
+import cors from 'cors';
+import ticketRoutes from './tickets/ticket.routes';
 
 const app = express();
+
+// Enable CORS for all origins (for dev)
+app.use(cors());
+
 app.use(express.json());
-app.use("/tickets", ticketRoutes);
+app.use('/tickets', ticketRoutes);
 
 export default app;
